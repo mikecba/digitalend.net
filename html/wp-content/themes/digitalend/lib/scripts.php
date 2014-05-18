b2b92431f360570e4970e216b7bce16135014470
mikecba <cbamike@gmail.com>
2014-05-18 11:54:05 -0400

    CSS Changes

--- html/wp-content/themes/digitalend/lib/scripts.php
+++ html/wp-content/themes/digitalend/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3c265870f68a1eb5f364de1fc2a40ecf');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c107b8d47d93b33fe48dff59de42e1f4');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
